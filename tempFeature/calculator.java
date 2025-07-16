cd83f002b1a3af14b9b468e4fa03f438a5d11e6d
Brandon Sims <109297671+b-sims@users.noreply.github.com>
2025-07-16 15:22:54 -0400

    Create feature calculator.java

---
+++ tempFeature/calculator.java
@@ -0,0 +1,36 @@
+/**
+ * A simple Calculator class that performs basic arithmetic operations.
+ *
+ * TODO:
+ * - Add support for more operations (e.g., exponentiation, modulo)
+ * - Add input validation and exception handling
+ * - Write unit tests
+ */
+
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;  // TODO: handle divide by zero
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(5, 3));
+        System.out.println("Subtract: " + calc.subtract(5, 3));
+        System.out.println("Multiply: " + calc.multiply(5, 3));
+        System.out.println("Divide: " + calc.divide(5, 3));
+    }
+}
